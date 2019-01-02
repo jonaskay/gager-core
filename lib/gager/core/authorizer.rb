6aaca694dd03d12d580ce5025ec311e90e02218f
Jonas Kay <jonaskay@iki.fi>
2019-01-02 21:04:56 +0200

    Ability to generate and store authorization with a code

--- lib/gager/core/authorizer.rb
+++ lib/gager/core/authorizer.rb
@@ -1,17 +1,33 @@
 require "googleauth"
+require "googleauth/stores/file_token_store"
 
 module Gager
   module Core
     class Authorizer
       OOB_URI = "urn:ietf:wg:oauth:2.0:oob"
 
-      def initialize(client_id, client_secret, scope, token_store: nil)
+      attr_reader :authorization
+
+      def initialize(client_id, client_secret, scope, user_id: "gager", token_store_file: nil)
         auth_id = Google::Auth::ClientId.new(client_id, client_secret)
-        @auth   = Google::Auth::UserAuthorizer.new(auth_id, scope, token_store)
+        if token_store_file
+          @token_store = Google::Auth::Stores::FileTokenStore.new(file: token_store_file)
+        end
+        @auth = Google::Auth::UserAuthorizer.new(auth_id, scope, @token_store)
+        @user_id = user_id
       end
 
       def authorization_url
         @auth.get_authorization_url(base_url: OOB_URI)
+      end
+
+      def authorization_code=(code)
+        args = {user_id: @user_id, code: code, base_url: OOB_URI}
+        @authorization = if @token_store
+          @auth.get_and_store_credentials_from_code(args)
+        else
+          @auth.get_credentials_from_code(args)
+        end
       end
     end
   end
--- spec/gager/core/authorizer_spec.rb
+++ spec/gager/core/authorizer_spec.rb
@@ -1,3 +1,21 @@
+RSpec.shared_examples "generate authorization" do
+  context "when code is valid" do
+    let(:code) { "MyCode" }
+
+    it "generates authorization" do
+      expect { subject }.to change { authorizer.authorization }
+    end
+  end
+
+  context "when code is invalid" do
+    let(:code) { "invalid" }
+
+    it "raises an error" do
+      expect { subject }.to raise_error("Authorization failed.")
+    end
+  end
+end
+
 RSpec.describe Gager::Core::Authorizer do
   let(:authorizer) { described_class.new("MyClientId", "MyClientSecret", "MyScope") }
 
@@ -15,5 +33,66 @@ RSpec.describe Gager::Core::Authorizer do
         "&scope=MyScope"
       )
     }
+  end
+
+  describe "#authorization_code=" do
+    before {
+      stub_request(:post, "https://oauth2.googleapis.com/token").with(
+        body: {
+          "client_id" => "MyClientId",
+          "client_secret" => "MyClientSecret",
+          "code" => "MyCode",
+          "grant_type" => "authorization_code",
+          "redirect_uri" => "urn:ietf:wg:oauth:2.0:oob"
+        }
+      ).to_return(
+        status: 200,
+        body: '{"access_token": "12345"}',
+        headers: {
+          "Content-Type" => "application/json; charset=utf-8"
+        }
+      )
+    }
+
+    before {
+      stub_request(:post, "https://oauth2.googleapis.com/token").with(
+        body: {
+          "client_id" => "MyClientId",
+          "client_secret" => "MyClientSecret",
+          "code" => "invalid",
+          "grant_type" => "authorization_code",
+          "redirect_uri" => "urn:ietf:wg:oauth:2.0:oob"
+        }
+      ).to_return(
+        status: 400,
+        body: "",
+        headers: {}
+      )
+    }
+
+    subject { authorizer.authorization_code = code }
+
+    context "when token_store is nil" do
+      include_examples "generate authorization"
+    end
+
+    context "when token_store is not nil" do
+      let(:token_store_file) { "spec/fixtures/tokens.yaml" }
+      let(:authorizer) { described_class.new("MyClientId", "MyClientSecret", "MyScope", token_store_file: token_store_file) }
+
+      include_examples "generate authorization"
+
+      context "when code is valid" do
+        let(:code) { "MyCode" }
+
+        before { File.delete(token_store_file) if File.exist?(token_store_file) }
+
+        it "saves credentials" do
+          expect { subject }.to change { File.exist?(token_store_file) }.to(true)
+        end
+
+        after { File.delete(token_store_file) if File.exist?(token_store_file) }
+      end
+    end
   end
 end
