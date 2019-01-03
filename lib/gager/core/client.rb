9db82ac43d7e375a39681635008834b4815e2aad
Jonas Kay <jonaskay@iki.fi>
2019-01-03 21:13:19 +0200

    Ability to make the reports request

--- lib/gager/core/client.rb
+++ lib/gager/core/client.rb
@@ -3,9 +3,29 @@ require 'google/apis/analyticsreporting_v4'
 module Gager
   module Core
     class Client
+      API = Google::Apis::AnalyticsreportingV4
+
       def initialize(authorization)
-        @service = Google::Apis::AnalyticsreportingV4::AnalyticsReportingService.new
+        @service = API::AnalyticsReportingService.new
         @service.authorization = authorization
+      end
+
+      def get_reports(requests_attributes)
+        report_requests = requests_attributes.map { |request| generate_report_request(request) }
+        request = API::GetReportsRequest.new(report_requests: report_requests)
+        @service.batch_get_reports(request)
+      end
+
+      private
+
+      def generate_report_request(attrs)
+        API::ReportRequest.new(
+          view_id: attrs["view_id"],
+          date_ranges: attrs["date_ranges"].map { |a| API::DateRange.new(start_date: a[0], end_date: a[1]) },
+          dimensions: attrs["dimensions"].map { |s| API::Dimension.new(name: s) },
+          metrics: attrs["metrics"].map { |s| API::Metric.new(expression: s) },
+          filters_expression: attrs["filters_expression"]
+        )
       end
     end
   end
--- spec/gager/core/client_spec.rb
+++ spec/gager/core/client_spec.rb
@@ -1,5 +1,73 @@
 RSpec.describe Gager::Core::Client do
-  it "initializes" do
-    expect(described_class.new({})).to be_instance_of(Gager::Core::Client)
+  let(:client) { described_class.new({}) }
+
+  describe "#get_reports" do
+    let(:report_requests) {
+      [
+        {
+          "view_id" => "123",
+          "date_ranges" => [["2015-06-15", "2015-06-30"]],
+          "dimensions" => ["ga:browser"],
+          "metrics" => ["ga:sessions"],
+          "filters_expression" => nil
+        }
+      ]
+    }
+
+    before {
+      stub_request(:post, "https://analyticsreporting.googleapis.com/v4/reports:batchGet")
+        .with(
+          body: {
+            "reportRequests" => [
+              {
+                "dateRanges" => [{ "endDate" => "2015-06-30", "startDate" => "2015-06-15" }],
+                "dimensions" => [{"name" => "ga:browser"}],
+                "filtersExpression" => nil,
+                "metrics" => [{ "expression" => "ga:sessions" }],
+                "viewId" => "123"
+              }
+            ]
+          }.to_json
+        )
+        .to_return(
+          status: 200,
+          body: {
+            "reports" => [
+              {
+                "columnHeader" => {
+                  "dimensions" => ["ga:browser"],
+                  "metricHeader" => {
+                    "metricHeaderEntries" => [{ "name" => "ga:sessions", "type" => "INTEGER" }]
+                  }
+                },
+                "data" => {
+                  "rows" => [
+                    {
+                      "dimensions" => ["Firefox"],
+                      "metrics" => [{ "values" => ["2161"] }]
+                    },
+                    {
+                      "dimensions" => ["Internet Explorer"],
+                      "metrics" => [{ "values" => ["1705"] }]
+                    }
+                  ],
+                  "totals" => [
+                    {
+                      "values" => ["3866"]
+                    }
+                  ]
+                }
+              }
+            ]
+          }.to_json,
+          headers: {
+            "Content-Type" => "application/json"
+          }
+        )
+    }
+
+    subject { client.get_reports(report_requests) }
+
+    it { is_expected.to be_instance_of(Google::Apis::AnalyticsreportingV4::GetReportsResponse) }
   end
 end
